2b6fe9fa4031c484dbeea2fd84141fdc806b006a
Endriw Villa <Endriw_villa@icloud.com>
2022-03-05 10:45:52 -0300

    Adicionado validação e melhorado o form

--- vue/js/index.js
+++ vue/js/index.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
-    /**
-     * Add evento de click
-     */
-    function onAdd() {
-          var $ul, li, $li, $label, $div, livro;
-          livro = $('.js-novo-jogo').val();
+/**
+ * Add evento de click
+ */
+function onAdd() {
+       var $ul, li, $li, $label, $div, jogo;
+       jogo = $('.js-novo-jogo').val();
 
-          // valida se “livro” está vazio
-          if (livro === '') {
-                 return;
-          }
+       // valida se “livro” está vazio
+       if (jogo === '') {
+              return;
+       }
 
-          $ul = $('ul');
-          $li = $('<li>').appendTo($ul);
-          $div = $('<div>')
-                 .addClass('checkbox')
-                 .appendTo($li);
+       $ul = $('ul');
+       $li = $('<li>').appendTo($ul);
+       $div = $('<div>')
+              .addClass('checkbox')
+              .appendTo($li);
 
-          $label = $('<label>').appendTo($div);
-          $('<input>')
-                 .attr('type', 'checkbox')
-                 .addClass('js-jogo')
-                 .attr('name', 'list')
-                 .click(toggleRemovido)
-                 .appendTo($label);
+       $label = $('<label>').appendTo($div);
+       $('<input>')
+              .attr('type', 'checkbox')
+              .addClass('js-jogo')
+              .attr('name', 'list')
+              .click(toggleRemovido)
+              .appendTo($label);
 
-          $label.append(livro);
-          $('.js-novo-jogo').val('');
-    }
+       $label.append(jogo);
+       $('.js-novo-jogo').val('');
+}
 
-    /**
-     * Evento de click do checkbox
-     */
-    function toggleRemovido(ev) {
-          var $el;
-          $el = $(ev.currentTarget);
-          $el.closest('li').toggleClass('removido');
-    }
-    $('.js-add').click(onAdd);
-    $('.js-jogo').click(toggleRemovido);
+/**
+ * Evento de click do checkbox
+ */
+function toggleRemovido(ev) {
+       var $el;
+       $el = $(ev.currentTarget);
+       $el.closest('li').toggleClass('removido');
+}
+$('.js-add').click(onAdd);
+$('.js-jogo').click(toggleRemovido);
 });
